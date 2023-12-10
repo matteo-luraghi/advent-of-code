2fd4fc5fefa6765d31eac73240bfc92a35b2c522
matteo-luraghi <matteoluraghi50@gmail.com>
2023-12-10 20:25:23 +0100

    almost done 7

--- 2023/7/7-a.go
+++ 2023/7/7-a.go
@@ -8,14 +8,31 @@ import (
 	"strings"
 )
 
+func smaller(a rune, b rune) bool {
+	lables := []rune{'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'}
+	return slices.Index(lables, a) > slices.Index(lables, b)
+}
+
+func order(hands []string) []string {
+	return hands
+}
+
+func points(table map[string]int, hands []string, startRank int, startRes int) (int, int) {
+	rank := startRank
+	res := startRes
+	for _, hand := range hands {
+		res += table[hand] * rank
+		rank--
+	}
+	return res, rank
+}
+
 func main() {
 	file, err := os.Open("7-a-test.txt")
 	if err != nil {
 		println(err)
 	}
 	defer file.Close()
-
-	res := 0
 
 	table := make(map[string]int)
 	var fiveOfKind []string
@@ -66,5 +83,20 @@ func main() {
 		}
 		table[hand] = bid
 	}
+
+	fiveOfKind = order(fiveOfKind)
+	fourOfKind = order(fourOfKind)
+	fullHouse = order(fullHouse)
+	threeOfKind = order(threeOfKind)
+	pair = order(pair)
+	high = order(high)
+
+	res, rank := points(table, fiveOfKind, len(table), 0)
+	res, rank = points(table, fourOfKind, rank, res)
+	res, rank = points(table, fullHouse, rank, res)
+	res, rank = points(table, threeOfKind, rank, res)
+	res, rank = points(table, pair, rank, res)
+	res, rank = points(table, high, rank, res)
+
 	println(res)
 }
